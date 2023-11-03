10f8a4c98fea3ba3379b7cd5d520f635d237d6f4
Daniel Tobon <Daniel_Tobon@epam.com>
2023-11-03 11:39:55 -0700

    Add project files.

---
+++ Task2/NumberParser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace Task2
+{
+    public class NumberParser : INumberParser
+    {
+        public int Parse(string stringValue)
+        {
+            if (stringValue == null)
+            {
+                throw new ArgumentNullException("Null exception in Parse method: stringValue cannot be null");
+            }
+            if (int.Parse(stringValue) > 2147483647 || int.Parse(stringValue) < -2147483648) 
+            {
+                throw new OverflowException($"Over flow exception in Parse method: '{stringValue}' is out of range");
+            }
+
+            // Try to parse the string as an integer.
+            if (!int.TryParse(stringValue, out int number))
+            {
+                throw new FormatException($"Could not parse the string '{stringValue}' as an integer.");
+            }
+            return number;
+
+            ////Invalid Number Format
+            //int n;
+            //try
+            //{
+            //    n = int.Parse(stringValue);
+            //}
+            //catch (FormatException e)
+            //{
+
+            //    throw new FormatException("Invalid format exception in Parse method: stringValue cannot be null",e);
+            //}
+
+            //throw new NotImplementedException();
+        }
+    }
+}
