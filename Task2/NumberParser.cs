8993c0f867a96a139fa39f5f1552d159f4220fe8
Daniel Tobon <Daniel_Tobon@epam.com>
2023-11-06 10:30:52 -0700

    Fix parser Number Parser class

--- Task2/NumberParser.cs
+++ Task2/NumberParser.cs
@@ -8,17 +8,35 @@ namespace Task2
         public int Parse(string stringValue)
         {
 
+            if (stringValue == null)
+            {
+                throw new ArgumentNullException("Null exception in Parse method: stringValue cannot be null");
+            }
+            if (Convert.ToInt32(stringValue) > int.MaxValue || Convert.ToInt32(stringValue) < int.MinValue)
+            {
+                throw new OverflowException($"Over flow exception in Parse method: '{stringValue}' is out of range");
+            }
+
+            int integerValue = 0;
+            bool isNegative = false;
             try
             {
-                if (stringValue == null)
+                // Check negative sign.
+                if (stringValue[0] == '-')
                 {
-                    throw new ArgumentNullException("Null exception in Parse method: stringValue cannot be null");
+                    isNegative = true;
+                    stringValue = stringValue.Substring(1);
                 }
-
-
-                if (Convert.ToInt32(stringValue) > int.MaxValue || Convert.ToInt32(stringValue) < int.MinValue)
+                //convert a string to an integer without using any native parsing of C#
+                foreach (char character in stringValue)
                 {
-                    throw new OverflowException($"Over flow exception in Parse method: '{stringValue}' is out of range");
+                    if (character >= '0' && character <= '9')
+                    {
+                        int digitValue = character - '0';
+
+                        integerValue *= 10;
+                        integerValue += digitValue;
+                    }
                 }
             }
             catch (FormatException ex)
@@ -29,8 +47,8 @@ namespace Task2
             {
                 Console.WriteLine(ex.Message);
             }
-
-            return Convert.ToInt32(stringValue);
+            // Return the integer value, with the negative sign if applicable.
+            return isNegative ? -integerValue : integerValue;
         }
     }
 }
